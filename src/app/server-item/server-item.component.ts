9cbbd16f890136fceb6047a8a403927b8dfc314a
Elvan H <elvanhd@gmail.com>
2022-04-08 07:31:05 +0700

    creating a server-item component

---
+++ src/app/server-item/server-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-item',
+  templateUrl: './server-item.component.html',
+  styleUrls: ['./server-item.component.css'],
+})
+export class ServerItemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
