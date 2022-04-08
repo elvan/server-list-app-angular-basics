c6fbe15e6ca7be1ae53d7b0638217a481c5f3909
Elvan H <elvanhd@gmail.com>
2022-04-08 07:42:08 +0700

    nesting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerItemComponent } from './server-item/server-item.component';
+import { ServerListComponent } from './server-list/server-list.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerItemComponent],
+  declarations: [AppComponent, ServerItemComponent, ServerListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/server-list/server-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-list',
+  templateUrl: './server-list.component.html',
+  styleUrls: ['./server-list.component.css'],
+})
+export class ServerListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
